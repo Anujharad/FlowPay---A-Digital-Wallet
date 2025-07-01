ba6852589602ba4ee38c063742d8baf732f85797
unknown <anujharad999@gmail.com>
2025-07-02 00:02:26 +0530

    initial commit

---
+++ paytm/backend/db.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+ mongoose.connect("mongodb+srv://anuj123:09876anuj@cluster0.8xivp.mongodb.net/paytm");
+ 
+ const userSchema= new mongoose.Schema({
+    username: String,
+    password: String,
+    firstName: String,
+    lastName: String,
+ });
+
+
+
+   const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model("User", userSchema);
+
+
+
+module.exports = {
+	User ,
+    Account
+};
